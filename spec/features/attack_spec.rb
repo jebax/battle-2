2b5b5b656dca4a7abc5415039033fc2e3ad56d02
jebax <joel.eliot.baxter@gmail.com>
2018-10-16 16:58:47 +0100

    passed tests

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,8 +1,8 @@
 describe Battle do
   feature 'attacking' do
-    scenario 'attacking player 2' do 
+    scenario 'attacking player 2' do
       sign_in_and_play
-      click_button 'Attack!'
+      click_button 'Attack Joel!'
       expect(page).to have_content "You attacked Joel!"
     end
   end
