160bd693257b1725c6bfcc3b4bacc7ac17f32514
jebax <joel.eliot.baxter@gmail.com>
2018-10-17 15:24:45 +0100

    added more tests for game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,10 +9,22 @@ describe Game do
     expect(game_1.player_2.name).to eq "Chloe"
   end
 
+  it 'should start with player_1 as the active player' do
+    expect(game_1.active_player).to eq player_1
+  end
+
   describe "#attack" do
     it 'should be able to attack a player' do
-      expect(game_1.player_2).to receive(:receive_hit)
+      expect(player_2).to receive(:receive_hit)
       game_1.attack
+    end
+  end
+
+  describe "#switch_turns" do
+    it 'can switch turns on attacking' do
+      allow(player_2).to receive(:receive_hit)
+      game_1.attack
+      expect(game_1.active_player).to eq player_2
     end
   end
 end
